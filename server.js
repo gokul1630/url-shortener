900814d952a170d3db67656f2dc930a9ee6479cf
aruntemme <arungauthamk@gmail.com>
2021-02-19 10:59:51 +0530

    added shortname and clicks feature

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 mongoose.connect('mongodb+srv://root:root@cluster0.krm1f.mongodb.net/testdb?retryWrites=true&w=majority', {
   useNewUrlParser:true,
   useUnifiedTopology: true
-}).then(console.log("hello"))
+}).then(console.log("DB connected"))
 
 app.set('view engine',  'ejs')
 
@@ -14,13 +14,22 @@ app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async(req, res)=>{
   const shortUrls = await ShortUrl.find()
-  console.log(shortUrls)
-  res.render('index', { shortUrls: shortUrls })
+  res.render('index', { shortUrls: shortUrls})
 })
 
 app.post('/shortUrls', async(req, res) => {
- await ShortUrl.create({full: req.body.fullUrl })
+ await ShortUrl.create({full: req.body.fullUrl, short: req.body.sURL})
  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req,res) => {
+  const shortUrl = await ShortUrl.findOne({short: req.params.shortUrl })
+  if(shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.click++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
 })
 
 app.listen(process.env.PORT || 5000);
