fc067a672df56f6236bbc9470e12835b3c83029f
gokul1630 <gokulgp3167@gmail.com>
2021-10-26 18:43:23 +0530

    refactor code with prettier

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 const UserSchema = new mongoose.Schema({
-    username:String,
-    password:String,
-}) ;
+  username: String,
+  password: String
+});
 UserSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model("User",UserSchema);
+module.exports = mongoose.model("User", UserSchema);
